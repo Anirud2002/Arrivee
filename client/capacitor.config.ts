0c9d99a27632880a551aaf4ec7c02bfd1c9597a5
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-04-01 14:11:50 -0600

    Project Setup

---
+++ client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.locationreminder',
+  appName: 'client',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
