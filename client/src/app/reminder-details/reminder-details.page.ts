83fe0231d7ad7a892a33f36821e8cb28c42cf7cf
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-04-05 00:06:09 -0600

    radius info on reminder detail page as well

--- client/src/app/reminder-details/reminder-details.page.ts
+++ client/src/app/reminder-details/reminder-details.page.ts
@@ -17,6 +17,7 @@ export class ReminderDetailsPage implements OnInit {
   selectedUnit: string = "m"; // set default unit to 'm' which is meters
   isAddRemInputOpen: boolean = false; // boolean to to store if user wants to create new reminder to not
   isAddingNewReminder: boolean = false; // boolean to store current state if user is adding rem or not
+  currentRadiusValue: number = 0;
   constructor(
     private modalController: ModalController,
   ) { }
@@ -35,7 +36,9 @@ export class ReminderDetailsPage implements OnInit {
   }
 
   returnUnit = (value: number) => {
-    return `${value}${this.selectedUnit}`
+    console.log(value)
+    this.currentRadiusValue = parseFloat(value.toFixed(1));
+    return `${this.currentRadiusValue}${this.selectedUnit}`
   }
 
   getMax(){
@@ -61,16 +64,35 @@ export class ReminderDetailsPage implements OnInit {
     let retVal;
     switch(this.selectedUnit){
       case SelectedUnit.km:
-        retVal = 0;
+        retVal = 0.5;
         break;
       case SelectedUnit.m:
         retVal = 10;
         break;
       case SelectedUnit.mil:
-        retVal = 0;
+        retVal = 0.5;
         break;
       default:
         retVal = 0;
+        break;
+    }
+    return retVal;
+  }
+
+  getStep(){
+    let retVal;
+    switch(this.selectedUnit){
+      case SelectedUnit.km:
+        retVal = 0.1;
+        break;
+      case SelectedUnit.m:
+        retVal = 10;
+        break;
+      case SelectedUnit.mil:
+        retVal = 0.1;
+        break;
+      default:
+        retVal = 1;
         break;
     }
     return retVal;
@@ -103,6 +125,14 @@ export class ReminderDetailsPage implements OnInit {
 
   enableSaveButton(){
     this.isAddingNewReminder = true;
+  }
+
+  /**
+   * THIS METHOD UPDATES currentRadiusValue WHEN DRAGGING THE RANGE. I DON'T KNOW WHY.
+   * WELL, IT WORKS SO...
+   *  */ 
+  updateRadius(e: any){
+  
   }
 
 }
