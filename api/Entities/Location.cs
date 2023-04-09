b5d35181f8bea8560e9148194755730280b24e96
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-04-09 13:45:44 -0600

    setting radius to double

--- api/Entities/Location.cs
+++ api/Entities/Location.cs
@@ -12,7 +12,7 @@ namespace api.Entities
         public string UserName { get; set; } = string.Empty;
         public string Title { get; set; } = string.Empty;
         public string StreetAddress { get; set; } = string.Empty;
-        public int Radius { get; set; }
+        public double Radius { get; set; }
         public string RadiusUnit { get; set; } = string.Empty;
         public List<Reminder> Reminders { get; set; } = new List<Reminder>();
 
