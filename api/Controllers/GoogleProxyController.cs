fca5bae8da6f075327eeeb1eebcf0a6f678980c6
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-04-19 22:40:50 -0600

    try adding google proxy to avoid apikey on client

---
+++ api/Controllers/GoogleProxyController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using SendGrid;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class GoogleProxyController : Controller
+    {
+        private readonly IConfiguration _config;
+        private string GoogleApiKey;
+        public GoogleProxyController(IConfiguration config)
+        {
+            _config = config;
+            GoogleApiKey = _config.GetValue<string>("Google:apiKey") ?? "";
+        }
+
+        [HttpGet("search-place/{place}")]
+        public async Task<ActionResult> SearchPlace(string place)
+        {
+            // initially get the place id
+            string? placeId = "";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(
+                    $"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={place}&inputtype=textquery&key={GoogleApiKey}"
+                );
+            if (response.IsSuccessStatusCode)
+            {
+                PlaceIdResponse? placeResponse = JsonConvert.DeserializeObject<PlaceIdResponse>(await response.Content.ReadAsStringAsync());
+                placeId = placeResponse?.Candidates[0].Place_Id;
+            }
+
+            // logic to get the places
+
+            HttpResponseMessage placesResponse = await client.GetAsync(
+                    $"https://maps.googleapis.com/maps/api/place/details/json?place_id={placeId}&key={GoogleApiKey}"
+                );
+
+            //var temp = await placesResponse.Content.ReadFromJsonAsync<PlaceDetails>();
+            dynamic? obj = JsonConvert.DeserializeObject(await placesResponse.Content.ReadAsStringAsync());
+
+            // Get the "result" object from the first element of the "candidates" array
+            dynamic? resultObj = obj?.candidates[0].result;
+
+            // Create a new object with only the desired fields
+            dynamic newObject = new
+            {
+                formatted_address = resultObj?.formatted_address,
+                name = resultObj.name,
+                geometry = resultObj.geometry
+            };
+
+
+            return new OkResult();
+        }
+    }
+
+    public class PlaceDetails
+    {
+        public string FormattedAddress { get; set; }
+        public string Name { get; set; }
+        public Cords Geometry { get; set; }
+        public class Cords
+        {
+            public Location Location { get; set; }
+        }
+        public class Location
+        {
+            public double Lat { get; set; }
+            public double Lng { get; set; }
+        }
+    }
+
+    
+
+    
+
+    public class PlaceIdResponse
+     {
+        public Candidate[] Candidates { get; set; }
+        public string Status { get; set; }
+
+        public class Candidate
+        {
+            public string Place_Id { get; set; }
+        }
+    }
+
+    
+
+}
+
