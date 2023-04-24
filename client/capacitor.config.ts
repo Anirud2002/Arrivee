ab2c992e2821e27cdf9a60c11507e5071f4ff184
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-04-23 23:57:48 -0600

    UI fixes for all the pages

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -1,10 +1,18 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { KeyboardResize, KeyboardStyle } from '@capacitor/keyboard';
 
 const config: CapacitorConfig = {
   appId: 'com.locationreminder.app',
   appName: 'locationreminder',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    Keyboard: {
+      resize: KeyboardResize.Body,
+      style: KeyboardStyle.Light,
+      resizeOnFullScreen: true,
+    },
+  }
 };
 
 export default config;
