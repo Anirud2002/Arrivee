3aabfe97e1c9428b408e6e33df4f919058dea4bc
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-05-25 21:32:56 -0600

    focusing the input in edit state for reminder modal

--- client/src/app/home/components/add-location-modal/components/reminder-item-modal/reminder-item-modal.component.ts
+++ client/src/app/home/components/add-location-modal/components/reminder-item-modal/reminder-item-modal.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { SharedModule } from '../../../../../shared/shared.module';
 import { Reminder } from '../../../../../_interfaces/Reminder.modal';
+import { IonInput } from '@ionic/angular';
 
 @Component({
   selector: 'app-reminder-item-modal',
@@ -12,6 +13,8 @@ import { Reminder } from '../../../../../_interfaces/Reminder.modal';
 export class ReminderItemModalComponent  implements OnInit {
   @Input() reminder: Reminder;
   @Input() index: number;
+  @ViewChild("remTitleInput", {static: false}) remTitleInput: IonInput;
+
   isInEditState: boolean = false;
   constructor() { }
 
@@ -19,6 +22,9 @@ export class ReminderItemModalComponent  implements OnInit {
 
   handleEdit(){
     this.isInEditState = true;
+    setTimeout(() => { // HACKY WAY TO SET THE INPUT TO FOCUS, IF FOUND BETTER WAY, REPLACE IT
+      this.remTitleInput.setFocus();
+    }, 200);
   }
 
   updateRemTitle(e){
