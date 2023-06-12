0349156f94c8648f1ec27b0d13b57a0687ab1934
anirudshrestha <ANIRUDSHRESTHA@boisestate.edu>
2023-06-11 22:18:42 -0600

    finally making the secrets manager work

--- api/Secrets.cs
+++ api/Secrets.cs
@@ -1,53 +1,51 @@
 ﻿using System;
+using System.Text.Json;
 using Amazon;
 using Amazon.SecretsManager;
 using Amazon.SecretsManager.Model;
 namespace api
 {
-	public class Secrets
-	{
-		public Secrets()
-		{
-		}
-
-		        /*
-         *	Use this code snippet in your app.
-         *	If you need more information about configurations or implementing the sample code, visit the AWS docs:
-         *	https://aws.amazon.com/developer/language/net/getting-started
-         */
-
-        public async Task<string> GetSecret()
-            {
-                string secretName = "ArriveeSecrets";
-                string region = "us-west-2";
-
-                IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName(region));
-
-                GetSecretValueRequest request = new GetSecretValueRequest
-                {
-                    SecretId = secretName,
-                    VersionStage = "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified.
-                };
-
-                GetSecretValueResponse response;
-
-                try
-                {
-                    response = await client.GetSecretValueAsync(request);
-                }
-                catch (Exception e)
-                {
-                    // For a list of the exceptions thrown, see
-                    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-                    throw e;
-                }
-
-                string secret = response.SecretString;
-
-            return secret;
-
-                // Your code goes here
-            }
+    public class Secrets
+    {
+        public Secrets()
+        {
         }
-}
 
+        public async Task<SecretsObject> GetSecret()
+        {
+            string secretName = "ArriveeSecrets";
+
+            IAmazonSecretsManager client = new AmazonSecretsManagerClient();
+
+            GetSecretValueRequest request = new GetSecretValueRequest
+            {
+                SecretId = secretName,
+            };
+
+            GetSecretValueResponse response;
+
+            try
+            {
+                response = await client.GetSecretValueAsync(request);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            var secretObj = JsonSerializer.Deserialize<SecretsObject>(response.SecretString);
+
+            return secretObj;
+        }
+
+        public class SecretsObject
+        {
+            public string GoogleApiKey { get; set; } = string.Empty;
+            public string DBAccessKey { get; set; } = string.Empty;
+            public string DBSecretAccessKey { get; set; } = string.Empty;
+            public string SmtpPassword { get; set; } = string.Empty;
+            public string SmtpHost { get; set; } = string.Empty;
+        }
+    }
+
+}
